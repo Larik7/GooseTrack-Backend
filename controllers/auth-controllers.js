856419524aaceaeee236cce2a5167d974ce37cc3
Larik7 <Larik777taganka@gmail.com>
2023-06-22 04:52:28 +0300

    start

---
+++ controllers/auth-controllers.js
@@ -0,0 +1,5 @@
+// const bcrypt = require("bcrypt");
+// const jwt = require("jsonwebtoken");
+// const {User} = require("../models/contact/user");
+// const {HttpError} = require("../helpers");
+// const {ctrlWrapper} = require("../decorators");
---
+++ middlewares/authenticate.js
@@ -0,0 +1,3 @@
+// const jwt = require("jsonwebtoken");
+// const {HttpError} = require("../helpers");
+// const {User} = require("../models/contact/user");
