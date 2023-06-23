a2224b92c383336b93707e9f7a0666be3506d040
zhdima <zhdima@gmail.com>
2023-06-23 10:25:41 +0300

    add reviews routes

---
+++ routes/api/reviews-routes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const reviewsController = require("../../controllers/reviews-controller");
+
+const schemas = require("../../schemas/reviews-schemas");
+
+const { validateBody } = require("../../decorators");
+
+const { authenticate } = require("../../middlewares");
+
+const router = express.Router();
+
+router.use(authenticate);
+
+router.get("/", reviewsController.getAllReviews);
+
+router.get("/own", authenticate, reviewsController.getReview);
+
+router.post("/own", authenticate, validateBody(schemas.reviewAddSchema), reviewsController.addReview);
+
+router.put("/own", authenticate, validateBody(schemas.reviewUpdateSchema), reviewsController.updateReview);
+
+router.patch("/own", authenticate, validateBody(schemas.reviewUpdateSchema), reviewsController.updateReview);
+
+router.delete("/own", authenticate, reviewsController.deleteReview);
+
+module.exports = router;
