e9fb2b88ac55c91c872abf47920fc6d46bd00b9b
Ahamoth <beeminer.store@gmail.com>
2023-06-25 23:03:44 +0300

    edit

--- decorators/index.js
+++ decorators/index.js
@@ -1,7 +1,9 @@
 const ctrlWrapper = require("./ctrlWrapper");
 const validateBody = require("./validateBody");
+const passport = require('./passport');
 
 module.exports = {
   ctrlWrapper,
   validateBody,
+  passport,
 };
