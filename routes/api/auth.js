4438367ed1beb64e4f767970fd5438d9f12da832
Ahamoth <beeminer.store@gmail.com>
2023-06-25 22:26:59 +0300

    edit

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const userCtrl = require('../../controllers/auth');
-const { validateBody, authenticate, uploder } = require('../../middlewares');
+const {
+  validateBody,
+  auth,
+  uploder,
+  uploadCloud,
+  passport,
+} = require('../../decorators');
 const {
   registrationSchema,
   loginSchema,
   emailSchema,
   updateUserSchema,
+  refreshSchema,
+  passSchema,
 } = require('../../schemas');
 
 //console.log(userCtrl);
 
 const router = express.Router();
+//------------------------google----------------------------------
+router.get(
+  '/google',
+  passport.authenticate('google', { scope: ['email', 'profile'] })
+);
+
+router.get(
+  '/google/callback',
+  passport.authenticate('google', { session: false }),
+  userCtrl.googleAuth
+);
+//--------------------------------------------------------------------
 router.post(
   '/registration',
   validateBody(registrationSchema),
@@ -19,20 +39,34 @@ router.post(
 
 router.post('/login', validateBody(loginSchema), userCtrl.login); //логінимось
 
+router.get('/login/:accessToken', userCtrl.loginWithToken);
+
+router.post('/refresh', validateBody(refreshSchema), userCtrl.refreshToken); // оновлюємо токен
+
 router.get('/verify/:verificationToken', userCtrl.verifyEmail); //верефікація пошти
 
 router.post('/verify', validateBody(emailSchema), userCtrl.resendVerifyEmail);
 
-router.get('/current', authenticate, userCtrl.getCurrent);
+router.get('/current', auth, userCtrl.getCurrent);
 
-router.post('/logout', authenticate, userCtrl.logout);
+router.post('/logout', auth, userCtrl.logout);
 
 router.patch(
   '/user',
-  authenticate,
+  auth,
   uploder.single('avatar'),
   validateBody(updateUserSchema),
-  userCtrl.updateUser
+  userCtrl.updateUserCloud
 );
+
+router.put(
+  '/user',
+  auth,
+  uploadCloud.single('avatar'),
+  validateBody(updateUserSchema),
+  userCtrl.updateUserCloud
+);
+
+router.patch('/user/pass', auth, validateBody(passSchema), userCtrl.checkPass);
 
 module.exports = router;
