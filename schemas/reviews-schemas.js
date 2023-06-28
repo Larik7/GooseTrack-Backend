44eae2e0346553e711bcf7d8047720803d7ff2fa
Larik7 <Larik777taganka@gmail.com>
2023-06-29 01:31:06 +0300

    fixed

--- schemas/reviews-schemas.js
+++ schemas/reviews-schemas.js
@@ -32,7 +32,7 @@ reviewSchema.post("save", handleMongooseError);
 
 const reviewAddSchema = Joi.object({
   comment: Joi.string(),
-  rating: Joi.number().integer().min(0).max(5).required().errorMessageLis({
+  rating: Joi.number().integer().min(0).max(5).required().messages({
     "any.required": "missing required rating field",
   }),
 });
